7f1b215148167760427e7eb36e3dfe3b1ced4298
Gilcimar Barros <gilcimar.barros@hotmail.com>
2022-02-01 22:49:34 -0300

    Create usandoVar1.js

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local) // a variavel esta visivel apenas dentro da funcao
+}
+
+teste()
+console.log(local) // a variavel nao esta visivel
